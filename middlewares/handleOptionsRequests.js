209363a3671c4da112e64ca59fffe5e8131d7c29
huswainamar@gmail.com <huswainamar@gmail.com>
2023-08-27 11:20:27 +0200

    fix options handling issue

--- middlewares/handleOptionsRequests.js
+++ middlewares/handleOptionsRequests.js
@@ -1,7 +1,7 @@
 const handleOptionsRequests = (req, res, next) => {
-  //const origin = req.headers.origin
+  const origin = req.headers.origin
   // Allow requests from any origin that is allowed by cors
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', origin);
   // Allow the specific methods requested
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   // Allow the specific headers requested
