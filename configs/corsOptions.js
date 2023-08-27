a9880e3853a5b8d77609475f1a7a219a5d5185c8
huswainamar@gmail.com <huswainamar@gmail.com>
2023-08-27 09:08:15 +0200

    allow all origins

--- configs/corsOptions.js
+++ configs/corsOptions.js
@@ -1,8 +1,9 @@
-const whitelist = ['http://localhost:7700', 'http://localhost:3000']
+// const whitelist = ['http://localhost:7700', 'http://localhost:3000']
 const corsOptions = {
-	origin: (origin, callback) => {
-		whitelist.find(site => site === origin) || !origin ? callback(null, true) : callback(new Error('no Permissions'))
-	},
+	origin: (origin, callback) => callback(null, true),
+	// origin: (origin, callback) => {
+	// 	whitelist.find(site => site === origin) || !origin ? callback(null, true) : callback(new Error('no Permissions'))
+	// },
 	'Access-Control-Allow-Credentials' :true,
 	optionsSuccessesStatus: 200,
 }
